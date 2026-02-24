4502aa1c076a449181df5bface48b0f17fcec4ef
Vitor Hugo <vitorhugodeassumcao@gmail.com>
2026-02-23 21:36:36 -0300

    67

---
+++ script/script.js
@@ -0,0 +1,9 @@
+// Não usar ESPAÇO, ACENTO, Ç, @#$%&*
+
+// Number - é uma função que temta converter um valor paasado para numero
+
+
+const numero = Number(prompt("Digite um Número:"))
+const numero2 = Number(prompt("Digite outro número"))
+alert(numero+numero2)
+
